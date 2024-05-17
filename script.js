cdc49b27583c16504a1423d7077d731d1913034f
Harsh Gakare <gakareharsh@gmail.com>
2024-05-17 10:09:08 +0530

    Made Loader Successfully

---
+++ script.js
@@ -0,0 +1,42 @@
+
+
+var t1 = gsap.timeline()
+t1.from(".line h1", {
+    y:150,
+    stagger:0.25,
+    duration:0.6,
+    delay:0.5
+})
+t1.from("#line1-part1",{
+    opacity: 0,
+    onStart:function(){
+    var h5timer = document.querySelector("#line1-part1 h5")
+    var grow = 0
+     setInterval(function(){
+        if(grow<100){
+            h5timer.innerHTML = grow++
+        }else{
+            h5timer.innerHTML = grow
+        }
+},35)
+    }
+})
+t1.to(".line h2",{
+    animationName:"anime",
+    opacity:1
+})
+t1.to("#loader",{
+    opacity: 0,
+    duration: 0.2,
+    delay: 4
+})
+t1.from("#page1",{
+    delay:0.2,
+    opacity: 0,
+    duration:0.5,
+    y:1600,
+    esay:Power4
+})
+t1.to("#loader",{
+    display:"none"
+})
